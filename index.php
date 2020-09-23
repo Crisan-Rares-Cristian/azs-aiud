fe7bc9609cea47ab914686e3eadf93b71bafe46a
Blackqwerty <rares504@gmail.com>
2020-09-23 19:59:49 +0300

    new website commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AZS Aiud</title>
+  <link rel="stylesheet" href="custom.css" >
+  <script src="https://kit.fontawesome.com/90773a75f5.js" crossorigin="anonymous"></script>
+</head>
+<body>
+   <nav class="navbar bg-dark">
+   <a href="index.php" class="btn btn-primary rounded">Home</a>
+   </nav>
+  <div class="container">
+    <h3 class="mt-3">Bine ați venit pe pagina Bisericii Aiud</h3>
+  
+  <section class="card card-body">
+    <img src="./img/church.jpg" alt="" class="card-img rounded">
+    <p><a href="https://www.facebook.com/Aiudadventist" target="_blank">Ne găsiți pe pagina de Facebook <i class="fab fa-facebook"></i></a></p>
+  </section>
+  </div>
+</body>
+</html>
