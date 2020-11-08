c17b59f0f6b191b332da30941b80b0829a11e183
Crisan-Rares-Cristian <47771026+Crisan-Rares-Cristian@users.noreply.github.com>
2020-11-08 20:16:50 +0200

    Added Seara 2 - Vie Imparatia ta LiVE

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <img src="./img/showcase.jpeg" alt="Biserica în acțiune" class="card-img rounded col-auto col-lg-5">
     <p><a href="https://www.facebook.com/Aiudadventist" target="_blank">Ne găsiți pe pagina de Facebook <i class="fab fa-facebook"></i></a></p>
   </section>
+  <section>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/TXAiPwuXrYQ/rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</section>
  <?php include('./inc/footer.php') ?>
 
   
