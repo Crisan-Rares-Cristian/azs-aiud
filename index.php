4c8bce5ed81d9f2d8822b5fe296884c8deb5e6a5
Crisan-Rares-Cristian <47771026+Crisan-Rares-Cristian@users.noreply.github.com>
2020-11-08 20:22:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 
  <?php include('./inc/header.php'); ?>
+<section>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/TXAiPwuXrYQ/rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</section>
   <section class="card card-body">
     <img src="./img/showcase.jpeg" alt="Biserica în acțiune" class="card-img rounded col-auto col-lg-5">
     <p><a href="https://www.facebook.com/Aiudadventist" target="_blank">Ne găsiți pe pagina de Facebook <i class="fab fa-facebook"></i></a></p>
   </section>
-  <section>
-   <iframe width="560" height="315" src="https://www.youtube.com/embed/TXAiPwuXrYQ/rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</section>
+  
  <?php include('./inc/footer.php') ?>
 
   
